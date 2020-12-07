9b089ec03768bb4d414946b0f9d87b6819be0ada
Kovács Endre <kovacsendre96@gmail.com>
2020-12-07 16:48:37 +0100

    bio

--- js.js
+++ js.js
@@ -24,10 +24,22 @@
       });
 
         $(document).ready(function(){
-          $(".member").hover(function(){
+          $(".member4").hover(function(){
             $(".member2, .member3, .member1").css({'filter': "blur(4px)"});
             }, function(){
             $(".member2,.member3,.member1").css({'filter': "blur(0)"});
           });
         });
      
+
+        $(document).ready(function(){
+          $(".members").hover(function(){
+            $(this).css({'filter':'grayscale(0)'});
+          }, function(){
+
+            $(this).css({'filter':'grayscale(60%)'});
+          
+          });
+        });
+
+      
